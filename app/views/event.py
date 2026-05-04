c49744938cbc975f14c6020f2499d45f3539d72a
Igor Bondarenko <bondarenko200021@mail.ru>
2026-05-04 16:48:35 +0700

    123

--- app/views/event.py
+++ app/views/event.py
@@ -6,6 +6,8 @@ from typing import Annotated
 from app.crud import event
 from app.schemas.event import ResponseOutAPIWithPlaces, ResponseEventWithPlaceById
 import uuid
+import aiohttp
+from app.models.event import EventStatus
 
 router = APIRouter(
     tags=["Events"],
@@ -52,3 +54,42 @@ async def get_events_by_id(
             detail="Событие не найдено",
         )
     return event_by_id
+
+
+async def make_session_for_request():
+    async with aiohttp.ClientSession() as session:
+        yield session
+
+
+@router.get("/events/{event_id}/seats")
+async def get_info_about_seats(
+    event_id: Annotated[uuid.UUID, Path(description="UUID события")],
+    session: AsyncSession = Depends(db_constructor.get_session),
+    http_session=Depends(make_session_for_request),
+):
+    current_event = await event.get_events_by_id_crud(
+        event_id=event_id,
+        session=session,
+    )
+    if current_event is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Событие не найдено",
+        )
+    if current_event.status != EventStatus.published:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Статус события не соответствует",
+        )
+    local_hostname: str = (
+        # "http://student-system-events-provider-web.student-system-events-provider.svc:8000"
+        "http://events-provider.dev-2.python-labs.ru"
+    )
+    api_path: str = f"/api/events/{event_id}/seats/"
+    request_url = local_hostname + api_path
+    response = await http_session.get(
+        url=request_url,
+        headers={"x-api-key": "SpRjfkM9eTa1Wqarl0xsup4L64FH65lQKVL8i7TGX0M"},
+    )
+    result = await response.json()
+    return result
