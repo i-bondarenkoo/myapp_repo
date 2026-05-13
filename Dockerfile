783108cc1f19d856295d8a34bb6431aa8b9acbca
Igor Bondarenko <bondarenko200021@mail.ru>
2026-05-13 15:59:08 +0700

    kdmfkopajmfpoksdjfposfwsf

--- Dockerfile
+++ Dockerfile
@@ -10,17 +10,12 @@
 
 # CMD ["python", "app.py"]
 
-FROM python:3.11-slim
+FROM python:3.12-slim
 
 WORKDIR /app
 
-COPY pyproject.toml uv.lock ./
-
-RUN pip install uv \
-    && uv sync --frozen --no-dev
-
 COPY . .
 
-RUN pip install sqlalchemy fastapi uvicorn aiohttp alembic asyncpg email-validator pydantic pydantic-settings
+RUN pip install --no-cache-dir .
 
 CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000"]
--- app/core/config.py
+++ app/core/config.py
@@ -3,14 +3,17 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
 
+    db_url: str | None = None
+    # db_echo: bool = False
+    db_echo: bool | None = None
+    events_api_key: str | None = None
+    events_base_url: str | None = None
+
     model_config = SettingsConfigDict(
         env_file=".env",
-        env_file_encoding="utf-8",
+        # env_file_encoding="utf-8",
+        extra="ignore",
     )
-    db_url: str
-    db_echo: bool = False
-    events_api_key: str
-    events_base_url: str
 
 
 settings = Settings()
